82bf898f61eed8571790e828dac956104fcde31d
singhtirupati27 <tirupati.singh@innoraft.com>
2023-09-29 20:11:26 +0530

    FT2023-423: Made changes in gulpfile code as per requirement.

--- web/themes/custom/bootstrap_subtheme/gulpfile.js
+++ web/themes/custom/bootstrap_subtheme/gulpfile.js
@@ -1,17 +1,84 @@
-const gulp = require('gulp');
-const sass = require('gulp-dart-sass');
-const srcPath = './scss/**/*.scss';
-const destPath = './css';
-sass.compiler = require('node-sass');
+let gulp = require('gulp'),
+  sass = require('gulp-sass')(require('sass')),
+  sourcemaps = require('gulp-sourcemaps'),
+  $ = require('gulp-load-plugins')(),
+  cleanCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  postcssInlineSvg = require('postcss-inline-svg'),
+  browserSync = require('browser-sync').create()
 
-// Style task.
-gulp.task('sass', function () {
-  return gulp.src(srcPath)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(destPath));
-});
+  pxtorem = require('postcss-pxtorem'),
+  postcssProcessors = [
+    postcssInlineSvg({
+      removeFill: true,
+      // paths: ['./node_modules/bootstrap-icons/icons']
+    }),
+    pxtorem({
+      propList: ['font', 'font-size', 'line-height', 'letter-spacing', '*margin*', '*padding*'],
+      mediaQuery: true
+    })
+  ];
 
-// Watch sass task.
-gulp.task('sass:watch', function () {
-  gulp.watch(srcPath, gulp.series('sass'));
-});
+const paths = {
+  scss: {
+    src: './scss/style.scss',
+    dest: './css',
+    watch: './scss/**/*.scss',
+    bootstrap_subtheme: './scss/blog.scss'
+  },
+  js: {
+    // bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    // popper: './node_modules/@popperjs/core/dist/umd/popper.min.js',
+    // barrio: '../../contrib/bootstrap_barrio/js/barrio.js',
+    dest: './js'
+  }
+}
+
+// Compile sass into CSS & auto-inject into browsers
+function styles() {
+  return gulp.src([paths.scss.src, paths.scss.bootstrap_subtheme])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: [
+        // './node_modules/bootstrap/scss',
+        // '../../contrib/bootstrap_barrio/scss',
+        './scss'
+      ]
+    }).on('error', sass.logError))
+    .pipe($.postcss(postcssProcessors))
+    .pipe(postcss([autoprefixer({
+      overrideBrowserslist: [
+        'Chrome >= 35',
+        'Firefox >= 38',
+        'Edge >= 12',
+        'Explorer >= 10',
+        'iOS >= 8',
+        'Safari >= 8',
+        'Android 2.3',
+        'Android >= 4',
+        'Opera >= 12']
+    })]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(cleanCss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(browserSync.stream())
+}
+
+// Static Server + watching scss/html files
+function serve() {
+  /*browserSync.init({
+    proxy: 'https://www.drupal.org',
+  })*/
+
+  gulp.watch([paths.scss.watch], styles).on('change', browserSync.reload)
+}
+
+const build = gulp.series(styles, gulp.parallel(serve))
+
+exports.styles = styles
+exports.serve = serve
+exports.default = build
